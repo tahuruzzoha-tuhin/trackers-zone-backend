ab671baf94255bdac0389d3342c426084d649d7e
Tahuruzzoha Tuhin <tzz.tuhin@gmail.com>
2023-03-01 16:28:37 +0600

    Soved issues

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const usr = "appUser";
-const pss = "g3dkzxwFd8h9OE8B";
-const uri = `mongodb+srv://${usr}:${pss}@cluster0.nb38n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nb38n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
